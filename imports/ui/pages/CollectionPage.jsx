bc2c9ee4300daa082f43ee83a382d599850f158d
Alan Leal <alanmeira@gmail.com>
2017-04-17 15:58:46 -0300

    fix reactivity

--- imports/ui/pages/CollectionPage.jsx
+++ imports/ui/pages/CollectionPage.jsx
@@ -6,36 +6,33 @@ import RecordList from '../components/RecordList';
 
 export default class CollectionPage extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { data: [] };
-  }
-  componentDidMount() {
-    Tracker.autorun(() => {
-      this.sub = Meteor.subscribe('Collection.all', this.props.collectionName, {
-        onReady: () => {
-          this.setState({ data: this.props.collection.find().fetch() });
-        },
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.sub) {
-      this.sub.stop();
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
     }
-  }
+    componentDidMount() {
+        Tracker.autorun(() => {
+            this.sub = Meteor.subscribe('Collection.all', this.props.collectionName);
+            this.setState({ data: this.props.collection.find().fetch() });
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <CreateRecord collection={this.props.collection} />
-        <RecordList
-          data={this.state.data}
-          collection={this.props.collection}
-          schema={this.props.collection.simpleSchema()}
-        />
-      </div>
-    );
-  }
+    componentWillUnmount() {
+        if (this.sub) {
+            this.sub.stop();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+              <CreateRecord collection={this.props.collection} />
+              <RecordList
+                data={this.state.data}
+                collection={this.props.collection}
+                schema={this.props.collection.simpleSchema()}
+                />
+            </div>
+        );
+    }
 }
